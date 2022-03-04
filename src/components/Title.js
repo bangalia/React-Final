279e453a974e82cca715d4b75a8fa656d9f83abe
bangalia <anitabangali@yahoo.com>
2022-03-03 20:32:49 -0600

    header component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Aloe Vera</h1>
+			<div className="Title-Subtitle">Your place for aloe vera knowledge</div>
+    </div>
+  )
+}
+
+export default Title
